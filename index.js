22630437d21b5db96c7606fe6c051bbe14b6ac84
Smiggy <70798673+Smigg-y@users.noreply.github.com>
2022-06-03 20:40:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { braillefy } = require('img2braille');
+
+var app = express();
+
+const port = process.env.PORT || 3000
+
+const asciiOpts = { 
+    dither: false,
+    invert: false,
+};
+
+app.set('json spaces', 4);
+app.use(express.static(`${__dirname}/`));
+app.listen(port);
+
+app.get('/image', async function(req, res) {
+    let image = req.query.url || null;
+
+    if(image === null) {
+        res.json({
+            'success': false,
+            'text': 'invalid url'
+        });
+    } else {
+        const result = await braillefy(image, 256, asciiOpts);
+        res.json({
+            'success': true,
+            'text': result
+        })
+    }
+});

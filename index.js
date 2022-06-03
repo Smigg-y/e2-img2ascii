8b2bd4a5d7588851d53825431d1ca1a9474004bc
Smiggy <70798673+Smigg-y@users.noreply.github.com>
2022-06-03 20:58:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ var app = express();
 
 const port = process.env.PORT || 3000
 
-const asciiOpts = { 
-    dither: false,
-    invert: false,
-};
-
 app.set('json spaces', 4);
 app.use(express.static(`${__dirname}/`));
 app.listen(port);
@@ -17,6 +12,8 @@ app.listen(port);
 app.get('/image', async function(req, res) {
     let image = req.query.url || null;
     let width = req.query.width || 30;
+    let doDither = req.query.dither || false;
+    let doInvert = req.query.invert || false;
     
     if(image === null) {
         res.json({
@@ -24,7 +21,12 @@ app.get('/image', async function(req, res) {
             'text': 'invalid url'
         });
     } else {
-        const result = await braillefy(image, width, asciiOpts);
+        let opts = {
+            dither: doDither,
+            invert: doInvert
+        };
+        
+        const result = await braillefy(image, width, opts);
         res.json({
             'success': true,
             'text': result
